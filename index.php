29056969298662f74f8ea79247b8843d81cec8d1
MaybeGoodDay <jeongathanasio@naver.com>
2022-01-13 00:13:10 +0900

    portfolio

---
+++ index.php
@@ -0,0 +1,110 @@
+<!DOCTYPE HTML>
+<!--
+	Paradigm Shift by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+-->
+<html>
+	<head>
+		<title>Let Me Introduce Myself</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+		<meta name="description" content="" />
+		<meta name="keywords" content="" />
+		<link rel="stylesheet" href="assets/css/main.css" />
+		<link rel="icon" href="/images/profile.jpg">
+	</head>
+	<body class="is-preload">
+
+		<!-- Wrapper -->
+			<div id="wrapper">
+
+				<!-- Intro -->
+					<section class="intro">
+						<header>
+							<h1>정민찬 / Jeong Min Chan </h1>
+							<p>Html Source by <a href="https://html5up.net">HTML5 UP</a></p>
+							<ul class="actions">
+								<li><a href="#first" class="arrow scrolly"><span class="label">Next</span></a></li>
+							</ul>
+						</header>
+						<div class="content">
+							<span class="image fill" data-position="center"><img src="images/asdfasfd.png" alt="" /></span>
+						</div>
+					</section>
+
+				<!-- Section -->
+					<section id="first">
+						<header>
+							<h2>Let me Introduce myself</h2>
+						</header>
+						<div class="content">
+							<p><strong>My name is Jeong Min Chan (정민찬).</strong> I like My family and My friends. And I like eat a delicious food. I'm happy every day. Because I laugh out loud "Ha, Ha, Ha!". :D</p>
+							<span class="image main"><img src="images/pic02.jpg" alt="" /></span>
+						</div>
+					</section>
+
+				<!-- Section -->
+					<section>
+						<header>
+							<h2>My Dream</h2>
+						</header>
+						<div class="content">
+							<p><strong>I want to be a Programmer.</strong> Because I like do Programming And I'm interested in Programming. Can I be a Programmer?</p>
+						</div>
+					</section>
+
+				<!-- Section -->
+					<section>
+						<header>
+							<h2>My favorite sentences.</h2>
+						</header>
+						<div class="content">
+							<Second.>First. <strong>"Don't be afraid of going slowly. But worry about stopping."</strong> Second. <strong>"With great power comes great responsibility."</strong> I want to live like this sentences.</p>
+						</div>
+					</section>
+
+				<!-- Section -->
+					<section>
+						<header>
+							<h2>Contact Number</h2>
+						</header>
+						<footer>
+							<ul class="items">
+								<li>
+									
+								</li>
+								<li>
+									<h3>Email</h3>
+									<a href="#">jeongathanasio@naver.com</a>
+								</li>
+								<li>
+									<h3>Phone</h3>
+									<a href="#">+82 10-3834-2753</a>
+								</li>
+								<li>
+									<h3>Social Media</h3>
+									<ul class="icons">
+										<li><a href="https://www.instagram.com/_jeong_minchan_/" class="icon brands fa-instagram"><span class="label">Instagram</span></a></li>
+										<li><a href="https://github.com/MaybeGoodDay52" class="icon brands fa-github"><span class="label">GitHub</span></a></li>
+									</ul>
+								</li>
+							</ul>
+						</footer>
+					</section>
+
+				<!-- Copyright -->
+					<div class="copyright">&copy; Untitled. All rights reserved. Design: <a href="https://html5up.net">HTML5 UP</a>.</div>
+
+			</div>
+
+		<!-- Scripts -->
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/jquery.scrolly.min.js"></script>
+			<script src="assets/js/browser.min.js"></script>
+			<script src="assets/js/breakpoints.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<script src="assets/js/main.js"></script>
+
+	</body>
+</html>
